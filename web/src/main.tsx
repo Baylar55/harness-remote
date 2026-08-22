44e97cd33d6d346dc833cc07d518e6501c9b1c37
giuliastro <giuliaccio@gmail.com>
2026-08-22 15:10:32 +0200

    style(v3): load final conversation control-plane overrides

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -20,6 +20,7 @@ import "./taskdesk-v3.css"
 import "./taskdesk-v3-unified.css"
 import "./taskdesk-run-review.css"
 import "./v3-polish.css"
+import "./conversation-control-plane-overrides.css"
 
 installAppPreferences()
 installCompletionAudioGuard()
