ba4b7c2fe6a4640b1dc2456466c0c79fe7a616cd
giuliastro <giuliaccio@gmail.com>
2026-08-24 09:11:17 +0200

    v3: load Session-first navigation override after HR3 styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -27,6 +27,7 @@ import "./v3-mobile-landscape-grid-fix.css"
 import "./v3-mobile-workspace-switcher-polish.css"
 import "./v3-mobile-a11y-fix.css"
 import "./v3-mobile-product-parity.css"
+import "./session-first-navigation.css"
 
 installAppPreferences()
 installCompletionAudioGuard()
